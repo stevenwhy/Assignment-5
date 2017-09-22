ade2093e636ccddfff7314fc8e3954afa8cd0431
Steven Y <stevenyildirim@gmail.com>
2017-09-22 17:43:45 -0400

    remove config

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb://stevenwhy:abc123@ds123124.mlab.com:23124/listings_steveny', //place the URI of your mongo database here.
+    }, 
+    googleMaps: {
+      key: 'AIzaSyDS91VH5IoW-m55AwNN6c7F4FRYs4GY1Bo'
+    },
+    port: 8080
+  };
